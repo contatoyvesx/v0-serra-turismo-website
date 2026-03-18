02e97c9603d7131617846cc7e0a5e72baecc5c29
contatoyvesx <contato.yvesx@gmail.com>
2026-03-18 18:18:24 -0300

    Add Google Tag Manager snippets

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,12 @@ import "./globals.css"
 const playfair = Playfair_Display({ subsets: ["latin"] })
 const inter = Inter({ subsets: ["latin"] })
 
+const googleTagManagerScript = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id=GTM-KD6TX9HZ'+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KD6TX9HZ');`
+
 export const metadata: Metadata = {
   title: "HA Serra Turismo",
   description:
@@ -38,7 +44,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
+      <head>
+        <script dangerouslySetInnerHTML={{ __html: googleTagManagerScript }} />
+      </head>
       <body className={`font-sans antialiased`}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KD6TX9HZ"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         {children}
         <Analytics />
       </body>
